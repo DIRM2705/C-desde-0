588590493592381d2bea91e15514d2494d375866
DIRM2705 <dirm2705@gmail.com>
2024-06-23 08:57:26 -0600

    Added examples

---
+++ Nivel 1/Variables/ejemplos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int num = 5;          // Guardamos un entero positivo
+    int num2 = -18;       // Guardamos un entero positivo
+    char num = '5';       // Guardamos un número
+    char letra = 'x';     // Guardamos una letra
+    char ascii = '?';     // Guardamos un caracter ASCII
+    float num = 2.5;      // Guardamos un flotante, menos precisión
+    double num2 = 1.3679; // Guardamos un doble, mayor precisión
+}
---
+++ Nivel 1/Variables/temperatura.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    float temperatura;
+
+    temperatura = 25;
+
+    if(temperatura >= 30)
+    {
+        printf("Hace calor");
+    }
+    else
+    {
+        printf("Hace buen clima");
+    }
+}
